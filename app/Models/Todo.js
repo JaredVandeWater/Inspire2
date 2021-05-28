3c7a17265b6826c96b6a8a664bacb2c1d6d21509
Jared VandeWater <jaredvande@gmail.com>
2021-05-28 12:38:52 -0600

    first commit

---
+++ app/Models/Todo.js
@@ -0,0 +1,15 @@
+export class Todo{
+    constructor(data){
+        completed = data.completed
+        id = data.id
+        user = data.user
+        description = data.description
+    }
+    
+    get TodoTemplate(){
+        /*html*/
+        `
+        `
+        // TODO paste todo template
+    }
+}
