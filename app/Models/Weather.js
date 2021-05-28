28f73696f57978f2382141ada8177674845d3635
Jared VandeWater <jaredvande@gmail.com>
2021-05-28 16:52:00 -0600

    clock

--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -1,12 +1,14 @@
 export class Weather{
     constructor(data){
-        this.type = data.weather.main
+        this.type = data.weather[0].main
         this.icon = `http://openweathermap.org/img/wn/${data.weather.icon}@2x.png`
         this.temp = data.main.temp
     }
 
     get Template(){
         return /*html*/ `
+        
         `
     }
+
 }
