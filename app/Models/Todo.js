0ea7d56386db93ed13d733eab2765691d1b854e5
Jared VandeWater <jaredvande@gmail.com>
2021-05-30 16:11:24 -0600

    s

--- app/Models/Todo.js
+++ app/Models/Todo.js
@@ -12,7 +12,7 @@ export class Todo{
             <input onclick="app.todosController.checkTodo('${this.id}')" type="checkbox" ${this.completed ? "checked" : ""} class="form-check-input" id="exampleCheck1">
             <label class="form-check-label sr-only" for="exampleCheck1">Todo Check</label>
             <li class="my-list-t mb-2">${this.description}</li>
-            <i class="fas fa-minus cursor-point text-danger my-auto" onclick="app.todosController.removeTodo('${this.id}')"></i>
+            <i class="fas fa-minus cursor-point text-danger mt-1" onclick="app.todosController.removeTodo('${this.id}')"></i>
         </div>
         `
         // TODO paste todo template
